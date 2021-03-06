023096afe193e637d04aeae654df3c889cc51dbc
Dan Kreiger <dan@dankreiger.com>
2021-03-06 06:48:08 +0100

    chore: cleanup

--- lib/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Context } from 'aws-lambda';
-declare type AwsLambdaContextMock = (props?: Record<keyof Context, any>) => Context;
-export declare const awsLambdaContextMock: AwsLambdaContextMock;
-export default awsLambdaContextMock;
